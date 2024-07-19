e8f5b195ab0286db8070a503d7e754cc5206a39e
Lana Kisel <svetlana.kisel.ca@gmail.com>
2024-07-19 15:49:30 -0700

    Adds Spinner when etEquipment query is not initialized yet

--- client/src/components/EquipmentByCategory.js
+++ client/src/components/EquipmentByCategory.js
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import EquipmentPictures from './EquipmentPictures';
+import { Spin } from 'antd';
 
 import { skipToken } from '@reduxjs/toolkit/query/react'
 import { useGetCategoryByNameQuery, useGetEquipmentsByCategoryQuery } from "../services/sportingGoodsApi"
@@ -8,12 +9,12 @@ import { useGetCategoryByNameQuery, useGetEquipmentsByCategoryQuery } from "../s
 const EquipmentByCategory = () => {
     let { category_name } = useParams()
     const { data: category } = useGetCategoryByNameQuery(category_name ?? skipToken)
-    const { data: equipments } = useGetEquipmentsByCategoryQuery(category?.id ?? skipToken)
+    const { data: equipments, isFetching: fetchingEquipments, isUninitialized:queryNotStarted } = useGetEquipmentsByCategoryQuery(category?.id ?? skipToken)
 
     const equipment = !!equipments && equipments.map(e => (
         <div key={e.id} className="equipment_pic">
             <EquipmentPictures pictures={e.pictures} />
-            <Link style={{marginLeft: 300}}to={`/equipments/${e.id}`}>{e.name}</Link>
+            <Link style={{ marginLeft: 'auto', marginRight: 'auto' }} to={`/equipments/${e.id}`}>{e.name}</Link>
             <br />
             <br />
         </div>
@@ -21,8 +22,11 @@ const EquipmentByCategory = () => {
 
     return (
         <div>
-            <h2 style = {{textAlign:"center"}}>{category?.name}</h2>
-            {equipment}
+            <h2 style={{ textAlign: "center" }}>{category_name}</h2>
+            {(equipments && equipments.length > 0 ?
+                equipment
+                :
+                (fetchingEquipments || queryNotStarted ? <Spin fullscreen /> : <h3 style={{ textAlign: "center" }}>There are no equipments for this category</h3>))}
         </div>
     )
 }
